c997a92ea0a2b34e867c70f8a927ef34e552ed7f
AnirudhHack <anirudhahack123@gmail.com>
2024-12-02 20:41:09 +0530

    Update ethAddress.js

--- frontend/constants/address/ethAddress.js
+++ frontend/constants/address/ethAddress.js
@@ -1,4 +1,4 @@
 export const ethAddress = {
-    "susdeVault":'0xd69bc314bdaa329eb18f36e4897d96a3a48c3eef',
+    "susdeVault":'0xf62eec897fa5ef36a957702aa4a45b58fe8fe312',
     "susde": "0x9d39a5de30e57443bff2a8307a4256c8797a3497"
 }
